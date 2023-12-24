7236e7bf1d94ba70820e62597ead7cf69ad69a87
daniellimadev <pdaniel643@gmail.com>
2023-12-23 23:28:01 -0300

    Refactoring entity Todo

--- src/main/java/com/github/daniellimadev/todolist/entity/Todo.java
+++ src/main/java/com/github/daniellimadev/todolist/entity/Todo.java
@@ -1,6 +1,7 @@
 package com.github.daniellimadev.todolist.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,20 +9,40 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "todos")
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Todo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private String description;
 
     private boolean accomplished;
 
     private int priority;
+
+    public Todo() {
+
+    }
+
+    public Todo(Long id, @NotBlank String name, @NotBlank String description, boolean accomplished, int priority) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.accomplished = accomplished;
+        this.priority = priority;
+    }
+
+
+    public Todo(String name, String description, boolean accomplished, int priority) {
+        this.name = name;
+        this.description = description;
+        this.accomplished = accomplished;
+        this.priority = priority;
+    }
 
 }
